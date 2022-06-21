092292f336bb70bee682eb302a04858b23f82cec
Prudhvi <medap@PrudhvisPC>
2022-06-21 17:33:37 +0530

    second commit

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ const makeAllPlays=()=>{
     element.classList.add("fa-play")
 })
 }
+document.getElementByClassName("home").addEventListener("click",reload());
 Array.from(document.getElementsByClassName("songItem")).forEach((element, i)=>{
     element.getElementsByTagName("h3")[0].innerHTML=songs[i].songName
     element.getElementsByTagName("p")[0].innerHTML=songs[i].songAuthor
